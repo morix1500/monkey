6ab9a1655b941c4b5900cc0f8926107ef8d3448b
morix1500 <rade1500@gmail.com>
2018-07-01 21:58:40 +0900

    .13

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -3,10 +3,10 @@ package lexer
 import "github.com/morix1500/monkey/token"
 
 type Lexer struct {
-	input string
-	position int // 入力における現在の位置（現在の文字を指し示す）
-	readPosition int // ここから読み込む位置（現在の文字の次）
-	ch byte // 現在検査中の文字
+	input        string
+	position     int  // 入力における現在の位置（現在の文字を指し示す）
+	readPosition int  // ここから読み込む位置（現在の文字の次）
+	ch           byte // 現在検査中の文字
 }
 
 func New(input string) *Lexer {
@@ -28,6 +28,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -48,9 +50,51 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
